941556ed85f1395a7a0d35fc4dd177e232ae8916
Ed Boorn <43168926+edboorn@users.noreply.github.com>
2020-04-30 16:39:29 +0100

    Updates

---
+++ precache-manifest.28ddcb123bd4ae8f9b6771cb6c0f7ef8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83571082dbab59fd8b78466d2480ae9c",
+    "url": "/covid-explorer/index.html"
+  },
+  {
+    "revision": "26ea96a849bfeebe0709",
+    "url": "/covid-explorer/static/css/main.1ccc8982.chunk.css"
+  },
+  {
+    "revision": "a4e48fa6e244c7920c3c",
+    "url": "/covid-explorer/static/js/2.6a5b74ff.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/covid-explorer/static/js/2.6a5b74ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26ea96a849bfeebe0709",
+    "url": "/covid-explorer/static/js/main.a5ca8cf6.chunk.js"
+  },
+  {
+    "revision": "25e890b3f89d42d6b696",
+    "url": "/covid-explorer/static/js/runtime-main.3ef7177c.js"
+  }
+]);
